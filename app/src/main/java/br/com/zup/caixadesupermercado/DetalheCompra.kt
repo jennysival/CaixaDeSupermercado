4536d7c8fba3918483df64420b726328c6cd26a2
jennysival <jeniffersival@gmail.com>
2022-06-02 12:01:48 -0300

    Implementar código na segunda activity

--- app/src/main/java/br/com/zup/caixadesupermercado/DetalheCompra.kt
+++ app/src/main/java/br/com/zup/caixadesupermercado/DetalheCompra.kt
@@ -1,11 +1,56 @@
 package br.com.zup.caixadesupermercado
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import br.com.zup.caixadesupermercado.databinding.ActivityDetalheCompraBinding
 
 class DetalheCompra : AppCompatActivity() {
+    private lateinit var binding: ActivityDetalheCompraBinding
+    private val botaoRefazerCompra: Button by lazy { findViewById(R.id.botao_refazer) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalhe_compra)
+
+        binding = ActivityDetalheCompraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        exibirDados()
+
+        botaoRefazerCompra.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.titulo_detalhe)
+    }
+
+    private fun exibirDados(){
+        binding.tvValorTotal.text = "O valor total é de: R$ ${recuperarDados()?.getValorTotal()}"
+    }
+
+    private fun recuperarDados(): Produto? {
+        val produto = intent.getParcelableExtra<Produto>(PROD)
+
+        if(produto != null) {
+            val valorTotal = calcularValor(produto.getQuantidade(), produto.getValor())
+            produto.setValorTotal(valorTotal)
+            return produto
+        }
+
+        return produto
+    }
+
+    private fun calcularValor(qtd: Double, valor: Double) = valor*qtd
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/br/com/zup/caixadesupermercado/Produto.kt
+++ app/src/main/java/br/com/zup/caixadesupermercado/Produto.kt
@@ -10,7 +10,14 @@ class Produto(
     private var valor: Double
     ): Parcelable {
 
+    private var valorTotal: Double = 0.0
+
     fun getNome() = this.nome
     fun getQuantidade() = this.quantidade
     fun getValor() = this.valor
+    fun getValorTotal() = this.valorTotal
+
+    fun setValorTotal(valor: Double){
+        valorTotal = valor
+    }
 }
